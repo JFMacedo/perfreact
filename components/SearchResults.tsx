e2dfd2cc4eb24780a0559838bea2f6260af8a1f4
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2021-09-25 08:23:02 -0300

    :package: added virtualization

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,5 @@
+import { List, ListRowRenderer } from "react-virtualized"
+
 import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
@@ -12,17 +14,36 @@ interface SearchResultsProps {
 }
 
 export function SearchResults({ results, totalPrice, onAddToWishList }: SearchResultsProps) {
+  const rowRender: ListRowRenderer = ({ index, key, style }) => {
+    return (
+      <div key={ key } style={ style }>
+        <ProductItem
+          product={ results[index] }
+          onAddToWishList={ onAddToWishList }
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>R$ { String(totalPrice.toFixed(2)).replace(".", ",") }</h2>
 
-      { results.map(product => (
+      <List
+        height={ 300 }
+        rowHeight={ 30 }
+        width={ 900 }
+        overscanRowCount={ 5 }
+        rowCount={ results.length }
+        rowRenderer={ rowRender }
+      />
+      {/* { results.map(product => (
         <ProductItem
           key={ product.id }
           product={ product }
           onAddToWishList={ onAddToWishList }
         />
-      )) }
+      )) } */}
     </div>
   )
 }
